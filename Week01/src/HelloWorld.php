d13c5b40067990b71ef2f9a4f6253b174451bb5b
Tony Moraes <tonylds1@gmail.com>
2019-05-24 23:38:13 -0300

    Organizing my project folders

---
+++ Week01/src/HelloWorld.php
@@ -0,0 +1,17 @@
+<?php
+namespace cs313\HelloWorld\HelloWorld;
+
+/**
+ * Class HelloWorld
+ * @package cs313\HelloWorld\HelloWorld
+ */
+final class HelloWorld
+{
+    /**
+     * @return string
+     */
+    public function greeting()
+    {
+        return "Hello World!";
+    }
+}
--- web/index.php
+++ web/index.php
@@ -1,5 +1,8 @@
 <?php
 
-  phpinfo();
+require_once '../vendor/autoload.php';
 
-?>
+$myFirstClass = new cs313\HelloWorld\HelloWorld\HelloWorld();
+$greetings = $myFirstClass->greeting();
+
+include hello.php;
