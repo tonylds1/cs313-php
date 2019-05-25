c3644980f1d01df1af80b82ea4739c2ad895480e
Tony Moraes <tonylds1@gmail.com>
2019-05-24 23:52:58 -0300

    teste over index

--- web/index.php
+++ web/index.php
@@ -4,5 +4,6 @@ require_once '../vendor/autoload.php';
 
 $myFirstClass = new cs313\HelloWorld\HelloWorld\HelloWorld();
 $greetings = $myFirstClass->greeting();
+var_dump($greetings); exit;
 
-include hello.php;
+include 'hello.php';
