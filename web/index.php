3d8c5b034b3cd4262f99ca92c6cf820535001d2b
Tony Moraes <tonylds1@gmail.com>
2019-05-24 23:53:54 -0300

    removing teste over index

--- web/index.php
+++ web/index.php
@@ -4,6 +4,5 @@ require_once '../vendor/autoload.php';
 
 $myFirstClass = new cs313\HelloWorld\HelloWorld\HelloWorld();
 $greetings = $myFirstClass->greeting();
-var_dump($greetings); exit;
 
 include 'hello.php';
