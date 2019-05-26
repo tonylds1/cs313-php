596369d0abad1fb7c5972920a18af1a56b8aaeb0
Tony Moraes <tonylds1@gmail.com>
2019-05-26 02:13:07 -0300

    using professor function

--- Condominium/src/Infrastructure/Peristence.php
+++ Condominium/src/Infrastructure/Peristence.php
@@ -7,16 +7,20 @@ class Persistence
 {
     public function getConection()
     {
-        $server = 'ec2-54-83-205-27.compute-1.amazonaws.com:5432';
-        $database = 'd8fg9vkqm6jfs';
-        $user = 'mrxfucsllijjls';
-        $password = "d32b81e28c9789e5dfeb8f32ef9f868aa22a438936475431f19ffc8aa32cb8f1";
-        $dsn = "pgsql:host=$server;dbname=$database";
-        $options = array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION);
-
         try {
+            $dbUrl = getenv('DATABASE_URL');
 
-            return new \PDO($dsn, $user, $password, $options);
+            $dbopts = parse_url($dbUrl);
+            $dbHost = $dbopts["host"];
+            $dbPort = $dbopts["port"];
+            $dbUser = $dbopts["user"];
+            $dbPassword = $dbopts["pass"];
+            $dbName = ltrim($dbopts["path"],'/');
+
+            $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+            $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+            return $db;
         } catch (\PDOException $ex) {
             echo $ex->getMessage();
             exit;
