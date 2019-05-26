df87380f8be4921f03f56145730dd7cf276f5405
Tony Moraes <tonylds1@gmail.com>
2019-05-26 19:43:50 -0300

    test with routing

---
+++ View/CondominiumUI/hello.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tony's Hello World</title>
+</head>
+<body>
+	<h1><?=$greeting ?></h1>
+    <p> To be sure that the page are rendering after the folder organization.</p>
+</body>
+</html>
