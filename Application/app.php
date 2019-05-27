b660aa9d9268dac82f047822e50c1f040fb1e55e
Tony Moraes <tonylds1@gmail.com>
2019-05-26 21:21:22 -0300

    test with routing

--- Application/app.php
+++ Application/app.php
@@ -3,7 +3,7 @@
 use Symfony\Component\Routing;
 
 $routes = new Routing\RouteCollection();
-$routes->add('hello', new Routing\Route('/hello/{name}', ['name' => 'World']));
+$routes->add('hello', new Routing\Route('/Condominium/hello'));
 $routes->add('bye', new Routing\Route('/bye'));
 
 return $routes;
