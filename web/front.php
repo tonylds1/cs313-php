aa6ea6449df85d62fbf64b42cf2d5779773fa57b
Tony Moraes <tonylds1@gmail.com>
2019-05-26 21:28:48 -0300

    test with routing

--- web/front.php
+++ web/front.php
@@ -15,6 +15,8 @@ $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
 try {
     var_dump(__DIR__.'/../View/%s.php');
+    $teste = $request->getPathInfo();
+    var_dump($teste);
     $route = extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
     var_dump($route); exit;
     ob_start();
