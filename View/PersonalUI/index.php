b0cb3a1d9a6428dbf1ba5f0fc5dcd044af72acd2
Tony Moraes <tonylds1@gmail.com>
2019-05-26 22:23:11 -0300

    Bringing Personal Page from week02

---
+++ View/PersonalUI/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tony´s Home Page for CS313 Web Engineering II</title>
+    <link href="https://fonts.googleapis.com/css?family=Bitter" rel="stylesheet"> 
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Dosis:400,700%7CBitter:400,400italic,700&amp;subset=latin,latin" media="all">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="../web/css/style.css">
+</head>
+<body>
+    <div class='container d-flex flex-column col-6  '>
+        <div id="header justify-content-end">
+            <a href="assignments.php" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">See Week Exercices</a>
+        </div>
+        <div id='my-picture'>
+            <div id='me-bra'> <img src="../web/img/me-bra.png" alt="Tony Moraes"></div>
+            <div id='me-usa'> <img src="../web/img/me-usa.png" alt="Tony Moraes"></div>
+            <div id='me-img'> <img src="../web/img/me.png" alt="Tony Moraes"></div>
+        </div>
+        <div id='slogan' class='justify-content-center'>
+            <p class='align-self-center'>HOME FOR WEB ENGINEERING II COURSE</p>
+        </div>
+        <div id='my-name' class='align-self-center'>
+            Tony Moraes
+        </div>
+        <div id='about-me' class='justify-content-center'>
+            <p class='text-center'>
+                Brazilian php coder. Eager for agile management. Actually Scrum Master at Vox Tecnologia.
+                I dont have enough time for games but I would love to spent sometimes with CS-Global.
+            </p>
+        </div>
+        <hr>
+        <?php echo $statement; ?>
+    </div>
+</body>
+</html>
