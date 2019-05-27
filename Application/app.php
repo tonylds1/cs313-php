cf365923bd2a8ce3293445ab661c36929bcac5fa
Tony Moraes <tonylds1@gmail.com>
2019-05-26 22:25:44 -0300

    Bringing Personal Page from week02

--- Application/app.php
+++ Application/app.php
@@ -4,7 +4,7 @@ use Symfony\Component\Routing;
 
 $routes = new Routing\RouteCollection();
 $routes->add('CondominiumUI/hello', new Routing\Route('/hello'));
-$routes->add('Personal/src/index', new Routing\Route('/personal'));
+$routes->add('Personal/src/personal', new Routing\Route('/personal'));
 $routes->add('PersonalUI/assignments', new Routing\Route('/assignments'));
 
 return $routes;
