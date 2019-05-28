8ae6c797c2a6667655a4f88926eea3a053fe27d1
Tony Moraes <tonylds1@gmail.com>
2019-05-27 23:44:29 -0300

    Trying to show the page

--- Application/app.php
+++ Application/app.php
@@ -7,7 +7,7 @@ $routes->add('CondominiumUI/hello', new Routing\Route('/hello'));
 
 $routes->add('personal_home', new Routing\Route('/personal', [
     '_controller' => 'cs313\Controllers\PersonalController::indexAction'
-]);
+]));
 
 $routes->add('PersonalUI/assignments', new Routing\Route('/assignments'));
 
