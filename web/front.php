8fa3a4c0604bb31ea157539b5778f0320cfc1ed1
Tony Moraes <tonylds1@gmail.com>
2019-05-27 21:03:40 -0300

    Trying Symfony/aPache

--- web/front.php
+++ web/front.php
@@ -1,29 +1,20 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
-
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\Routing;
 
 $request = Request::createFromGlobals();
 $routes = include __DIR__.'/../Application/app.php';
 
 $context = new Routing\RequestContext();
-$context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
-try {
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
+$controllerResolver = new ControllerResolver();
+$argumentResolver = new ArgumentResolver();
 
-    ob_start();
-    include sprintf(__DIR__.'/../View/%s.php', $_route);
-
-    $response = new Response(ob_get_clean());
-} catch (Routing\Exception\ResourceNotFoundException $exception) {
-    $response = new Response('Not Found', 404);
-} catch (Exception $exception) {
-    $response = new Response('An error occurred', 500);
-}
+$framework = new Simplex\Framework($matcher, $controllerResolver, $argumentResolver);
+$response = $framework->handle($request);
 
 $response->send();
