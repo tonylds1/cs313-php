32e2a62f0ed26b3c0f57510bb3d90c1237963778
Tony Moraes <tonylds1@gmail.com>
2019-05-27 23:20:07 -0300

    Trying to show the page

--- Application/app.php
+++ Application/app.php
@@ -3,12 +3,12 @@
 use Symfony\Component\Routing;
 
 $routes = new Routing\RouteCollection();
-$routes->add('CondominiumUI/hello', new Routing\Route('/hello'));
+//$routes->add('CondominiumUI/hello', new Routing\Route('/hello'));
 
 $routes->add('personal_home', new Routing\Route('/personal'),[
     '_controller' => 'cs313\Controllers\PersonalController::indexAction'
 ]);
 
-$routes->add('PersonalUI/assignments', new Routing\Route('/assignments'));
+//$routes->add('PersonalUI/assignments', new Routing\Route('/assignments'));
 
 return $routes;
