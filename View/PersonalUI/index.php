a0aea8fd606abff1c2c420612ba9357750b05d67
Tony Moraes <tonylds1@gmail.com>
2019-05-27 23:52:22 -0300

    Trying to show the page

--- View/PersonalUI/index.php
+++ View/PersonalUI/index.php
@@ -27,8 +27,8 @@ $statement = 'Hi! I am the server in '. $date->getTimezone()->getName()
     <title>Tony´s Home Page for CS313 Web Engineering II</title>
     <link href="https://fonts.googleapis.com/css?family=Bitter" rel="stylesheet"> 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Dosis:400,700%7CBitter:400,400italic,700&amp;subset=latin,latin" media="all">
-    <link rel="stylesheet" href="../../web/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../../web/css/style.css">
+    <link rel="stylesheet" href="../web/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../web/css/style.css">
 </head>
 <body>
     <div class='container d-flex flex-column col-6  '>
@@ -36,9 +36,9 @@ $statement = 'Hi! I am the server in '. $date->getTimezone()->getName()
             <a href="assignments" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">See Week Exercices</a>
         </div>
         <div id='my-picture'>
-            <div id='me-bra'> <img src="../../web/img/me-bra.png" alt="Tony Moraes"></div>
-            <div id='me-usa'> <img src="../../web/img/me-usa.png" alt="Tony Moraes"></div>
-            <div id='me-img'> <img src="../../web/img/me.png" alt="Tony Moraes"></div>
+            <div id='me-bra'> <img src="../web/img/me-bra.png" alt="Tony Moraes"></div>
+            <div id='me-usa'> <img src="../web/img/me-usa.png" alt="Tony Moraes"></div>
+            <div id='me-img'> <img src="../web/img/me.png" alt="Tony Moraes"></div>
         </div>
         <div id='slogan' class='justify-content-center'>
             <p class='align-self-center'>HOME FOR WEB ENGINEERING II COURSE</p>
