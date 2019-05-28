75de2c21bbf01d2fa535ddaeb57137c0827143bd
Tony Moraes <tonylds1@gmail.com>
2019-05-27 22:53:24 -0300

    Test with Controller

--- Controllers/PersonalController.php
+++ Controllers/PersonalController.php
@@ -2,11 +2,12 @@
 
 namespace cs313\Controllers;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class PersonalController
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $response = new Response(include '../View/PersonalUI/index.php');
         $response->send();
