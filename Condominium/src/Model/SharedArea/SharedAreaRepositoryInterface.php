9e3132540362568ed94a384661562f1787ca6e09
Tony Moraes <tonylds1@gmail.com>
2019-06-02 00:58:56 -0300

    [FIX] SharedAreaList should receive the repository

--- Condominium/src/Model/SharedArea/SharedAreaRepositoryInterface.php
+++ Condominium/src/Model/SharedArea/SharedAreaRepositoryInterface.php
@@ -5,6 +5,6 @@ namespace cs313\Condominium\Model\SharedArea;
 interface SharedAreaRepositoryInterface
 {
     public function findAll(SharedAreaDTO $filter): array;
-    public function findOne(SharedAreaDTO $filter): SharedArea;
+    public function findOne(SharedAreaDTO $filter): ?SharedArea;
     public function findById(int $id): SharedArea;
 }
