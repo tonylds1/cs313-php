c5f1f1a7dcc5f29236ca55ae4e1d9c6518db2b3f
Tony Moraes <tonylds1@gmail.com>
2019-06-13 15:59:57 -0300

    Trying Modularize view

--- Controllers/AbstractSimplexController.php
+++ Controllers/AbstractSimplexController.php
@@ -3,11 +3,11 @@
 namespace cs313\Controllers;
 
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Route;
+use View\IRender;
 
 abstract class AbstractSimplexController
 {
-    protected function render(\IRender $render, array $parameters = [])
+    protected function render(IRender $render, array $parameters = [])
     {
         ob_start();
         extract($parameters, EXTR_SKIP);
