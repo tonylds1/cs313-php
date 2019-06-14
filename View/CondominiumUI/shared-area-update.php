26930d5b596afb33d006aab1ff76b200969905b5
Tony Moraes <tony@voxtecnologia.com.br>
2019-06-14 17:58:51 -0300

    using bootstrap

--- View/CondominiumUI/shared-area-update.php
+++ View/CondominiumUI/shared-area-update.php
@@ -1,35 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tony´s Home Page for CS313 Web Engineering II</title>
-    <link href="https://fonts.googleapis.com/css?family=Bitter" rel="stylesheet"> 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Dosis:400,700%7CBitter:400,400italic,700&amp;subset=latin,latin" media="all">
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/style.css">
-</head>
-<body>
-    <div class='container d-flex flex-column col-6  '>
-        <div id="header justify-content-end">
-            <a href="assignments" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">See Week Exercices</a>
+<h1>UPDATE SHARED AREA</h1>
+
+<form id="new-shared-area" method="post" action="/front.php/shared-area/save/<?=$sharedArea->getId(); ?>">
+    <div class="row">
+        <div class="col">
+            <input type="text" class="form-control" name='name' placeholder="Description"
+            value="<?=$sharedArea->getName(); ?>">
         </div>
-
-        <div class="justify-content-center search-box">
-            <h1>UPDATE SHARED AREA</h1>
-
-            <form id="new-shared-area" method="post" action="/front.php/shared-area/save/<?=$sharedArea->getId(); ?>">
-                <div class="row">
-                    <div class="col">
-                        <input type="text" class="form-control" name='name' placeholder="Description"
-                        value="<?=$sharedArea->getName(); ?>">
-                    </div>
-                    <div class="col">
-                        <button type="submit" class="btn btn-primary">Save</button>
-                    </div>
-                </div>
-            </form>
+        <div class="col">
+            <button type="submit" class="btn btn-primary">Save</button>
         </div>
     </div>
-</body>
-</html>
+</form>
--- View/CondominiumUI/shared-area.php
+++ View/CondominiumUI/shared-area.php
@@ -1,29 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tony´s Home Page for CS313 Web Engineering II</title>
-    <link href="https://fonts.googleapis.com/css?family=Bitter" rel="stylesheet"> 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Dosis:400,700%7CBitter:400,400italic,700&amp;subset=latin,latin" media="all">
-    <link rel="stylesheet" href="/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/style.css">
-</head>
-<body>
-    <div class='container d-flex flex-column col-6  '>
-        <div id="header justify-content-end">
-            <a href="assignments" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">See Week Exercices</a>
-        </div>
 
-        <div class="justify-content-center search-box">
-            <h1><?=$sharedArea->getName(); ?></h1>
-            <div id="header justify-content-end">
-                <a href="/front.php/week05" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Back</a>
-                <a href="/front.php/shared-area/delete/<?=$sharedArea->getId(); ?>" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Delete</a>
-                <a href="/front.php/shared-area/update/<?=$sharedArea->getId(); ?>" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Update</a>
-            </div>
+<h1><?=$sharedArea->getName(); ?></h1>
+<div id="header justify-content-end">
+    <a href="/front.php/shared-area/list" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Back</a>
+    <a href="/front.php/shared-area/delete/<?=$sharedArea->getId(); ?>" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Delete</a>
+    <a href="/front.php/shared-area/update/<?=$sharedArea->getId(); ?>" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Update</a>
+</div>
 
-        </div>
-    </div>
-</body>
-</html>
