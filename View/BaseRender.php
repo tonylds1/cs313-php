2883adfea28f6f616044cbd80c3411add6e07e35
Tony Moraes <tony@voxtecnologia.com.br>
2019-06-14 15:33:11 -0300

    using bootstrap

--- View/BaseRender.php
+++ View/BaseRender.php
@@ -28,9 +28,9 @@ class BaseRender implements IRender
             <title>Tony´s Home Page for CS313 Web Engineering II</title>
             <link href="https://fonts.googleapis.com/css?family=Bitter" rel="stylesheet">
             <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Dosis:400,700%7CBitter:400,400italic,700&amp;subset=latin,latin" media="all">
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+            <link rel="stylesheet" href="/css/bootstrap.min.css">
+            <script src="/js/jquery-3.4.1.min.js"></script>
+            <script src="/js/bootstrap.min.js"></script>
         </head>
         <body>
         <?php include 'nav.phtml'; ?>
