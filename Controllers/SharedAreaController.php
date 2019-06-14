ab2f22319a7f25df0b42594903313493cbafb1ca
Tony Moraes <tony@voxtecnologia.com.br>
2019-06-14 17:56:28 -0300

    using bootstrap

--- Controllers/SharedAreaController.php
+++ Controllers/SharedAreaController.php
@@ -5,7 +5,9 @@ namespace cs313\Controllers;
 use cs313\Condominium\Infrastructure\SharedAreaRepository;
 use cs313\Condominium\Model\SharedArea\SharedAreaDTO;
 use cs313\Condominium\Model\SharedArea\SharedAreaList;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 use View\BaseRender;
+use View\SharedAreaRender;
 use View\SharedAreasRender;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -37,15 +39,34 @@ class SharedAreaController extends  AbstractSimplexController
         }
     }
 
+    public function sharedAreaAction(Request $request)
+    {
+        try {
+            $id = empty($request->get('id')) ? null : (int)$request->get('id');
+            $sharedArea = (new SharedAreaRepository())->findById($id);
+
+            if (!$sharedArea) {
+                return new Response('No Shared Area with that Id');
+            }
+
+            $render = new SharedAreaRender();
+            $baseRender = new BaseRender($render, ['sharedArea' => $sharedArea]);
+
+            return new Response($baseRender->render());
+        } catch (\Throwable $t) {
+            return new RedirectResponse('/front.php/shared-area/list');
+        }
+    }
+
     public function sharedAreaDeleteAction(Request $request)
     {
         try {
             $id = empty($request->get('id')) ? null : (int) $request->get('id');
             (new SharedAreaRepository())->delete($id);
 
-            return new RedirectResponse('/front.php/shared-area');
+            return new RedirectResponse('/front.php/shared-area/list');
         } catch (\Throwable $t) {
-            return new RedirectResponse('/front.php/shared-area');
+            return new RedirectResponse('/front.php/shared-area/list');
         }
     }
 
@@ -60,7 +81,7 @@ class SharedAreaController extends  AbstractSimplexController
 
             return new Response($baseRender->render());
         } catch (\Throwable $t) {
-            return new RedirectResponse('/front.php/shared-area');
+            return new RedirectResponse('/front.php/shared-area/list');
         }
     }
 
@@ -74,14 +95,14 @@ class SharedAreaController extends  AbstractSimplexController
             if ($id) {
                 $repository->update($sharedArea);
 
-                return new RedirectResponse('/front.php/shared-area');
+                return new RedirectResponse('/front.php/shared-area/list');
             }
 
             $repository->insert($sharedArea);
 
-            return new RedirectResponse('/front.php/shared-area');
+            return new RedirectResponse('/front.php/shared-area/list');
         } catch (\Throwable $t) {
-            return new RedirectResponse('/front.php/shared-area');
+            return new RedirectResponse('/front.php/shared-area/list');
         }
     }
 }
